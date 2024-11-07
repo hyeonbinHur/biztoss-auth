5e6dad9da5e184b16d110ddbca7f0077cb1272be
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-11-08 01:36:28 +0900

    Style: footer styling complete
    Style: 웹사이트의 footer를 구현후 스타일링 완성.

--- component/Footer/Footer.tsx
+++ component/Footer/Footer.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer--container">
+        <div className="footer--policy">
+          이용약관 <span className="footer--policy__2"> 개인정보 처리방침</span>
+        </div>
+
+        <p className="footer--site-name">비즈토스</p>
+
+        <p className="footer--company-name">이숲컴퍼니</p>
+
+        <ul className="footer--company-ul">
+          <li>서울 특별시 관악구 호암로 24길 6</li>
+          <li>설립일 : 2024.03.11</li>
+          <li>대표 : 이상철</li>
+          <li>사업자등록증번호 : 573-05-02836</li>
+          <li>통신판매신고번호 : 2024-서울관악-1651</li>
+          <li>고객센터 : 070-8065-0953</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
